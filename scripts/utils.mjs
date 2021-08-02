9928496064afc568067a23523cdf128a8c8a6c60
Andrew Boyd <andrew@wearebraid.com>
2021-08-02 10:11:35 -0400

    use synchronous fs calls
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
-import fs from 'fs/promises'
+import fs from 'fs'
 import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 
@@ -11,27 +11,27 @@ const packagesDir = resolve(__dirname, '../packages')
 /**
  * Get the available packages from the packages directory.
  */
-export async function getPackages() {
-  const availablePackages = await fs.readdir(packagesDir);
+export function getPackages() {
+  const availablePackages = fs.readdirSync(packagesDir);
   return availablePackages
 }
 
 /**
  * Determine the correct build order of a provided array of packages
  */
- export async function getBuildOrder(packages = [], orderedPackages = []) {
+ export function getBuildOrder(packages = [], orderedPackages = []) {
   for (const p of packages) {
     packages.shift()
-    const dependencies = await getPackageDependencies(p)
+    const dependencies = getPackageDependencies(p)
     for (const d of dependencies) {
       if (!orderedPackages.includes(d)) {
-        const nestedDeps = await getBuildOrder([d], orderedPackages)
+        const nestedDeps = getBuildOrder([d], orderedPackages)
         orderedPackages = [...nestedDeps, d]
       }
       orderedPackages = [...orderedPackages, p]
     }
     if (packages && packages.length) {
-      return await getBuildOrder(packages, orderedPackages)
+      return getBuildOrder(packages, orderedPackages)
     }
   }
   return orderedPackages
@@ -40,13 +40,12 @@ export async function getPackages() {
 /**
  * Provide array of dependency packages for a provided package
  */
-export async function getPackageDependencies (p) {
-  const packageJSONRaw = await fs.readFile(resolve(__dirname, `../packages/${p}/package.json`));
+export function getPackageDependencies (p) {
+  const packageJSONRaw = fs.readFileSync(resolve(__dirname, `../packages/${p}/package.json`));
   const packageJSON = JSON.parse(packageJSONRaw)
   const dependencies = packageJSON.dependencies ? packageJSON.dependencies : []
   const devDependencies = packageJSON.devDependencies ? packageJSON.devDependencies : []
-  const deps = [...getFKDependenciesFromObj(dependencies), ...getFKDependenciesFromObj(devDependencies)]
-  return deps
+  return [...getFKDependenciesFromObj(dependencies), ...getFKDependenciesFromObj(devDependencies)]
 }
 
 /**
