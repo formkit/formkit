c6222074a423c78a7318f6546f1829a5dc5e7d98
Andrew Boyd <andrew@wearebraid.com>
2021-08-03 13:41:16 -0400

    publish script: tracks version changes in dependencies
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -21,18 +21,26 @@
 
 import cac from 'cac'
 import prompts from 'prompts'
+import chalk from 'chalk'
 // import fs from 'fs/promises'
 // import execa from 'execa'
 // import { dirname, resolve } from 'path'
 // import { fileURLToPath } from 'url'
 import {
+  checkDependsOn,
   getPackages,
+  getPackageFromFS,
+  checkGitCleanWorkingDirectory,
+  getLatestPackageCommits,
+  getFKDependenciesFromObj,
   getDependencyTree,
   drawDependencyTree,
+  flattenDependencyTree,
+  isAlphaNumericVersion,
   msg
 } from './utils.mjs'
 import { buildAllPackages } from './build.mjs'
-import { getBuildOrder } from './utils.mjs'
+import { getPackageJSON } from './utils.mjs'
 
 // const __filename = fileURLToPath(import.meta.url)
 // const __dirname = dirname(__filename)
@@ -40,70 +48,201 @@ import { getBuildOrder } from './utils.mjs'
 // const packagesDir = resolve(__dirname, '../packages')
 // const rollup = `${rootDir}/node_modules/.bin/rollup`
 
+const allPackages = []
+let toBePublished = []
+const prePublished = {}
+
 /**
  * Main entry point to the build process
  */
 async function publishPackages () {
-  await confirmBuildStatus()
-  const packages = getBuildOrder(await getPackagesToPublish())
-  if (!packages || !packages.length) return
-  const dependentTree = await getDependencyTree(packages, true)
-  msg.label('Dependent tree:')
-  drawDependencyTree(dependentTree)
-}
+  console.log(checkGitCleanWorkingDirectory())
 
-/**
- * Confirm with user that all target packages are built
- */
-async function confirmBuildStatus () {
-  const { hasBuilt } = await prompts({
+  allPackages.push(...await getPackages())
+  const shouldBuild = await buildAllPackagesConsent()
+  if (!shouldBuild) {
+    return msg.error('Publish aborted.')
+  }
+  msg.info('🌎 Building all packages.')
+  // await buildAllPackages(allPackages)
+  await getChangedDist()
+  msg.headline(`\nThe following packages have changes when diffed with their last published version.
+Any dependent packages will also require publishing to include dependency changes:`
+  )
+  const dependencyTree = await getDependencyTree(toBePublished, true)
+  drawDependencyTree(dependencyTree)
+  msg.info(`\nPackages will be published in the following order:`)
+  toBePublished = await getPublishOrder(toBePublished)
+  console.log(toBePublished, '\n')
+
+  const { confirmBuild } = await prompts({
     type: 'confirm',
-    name: 'hasBuilt',
-    message: 'Have you already built the packages you would like to publish?'
+    name: 'confirmBuild',
+    message: `Continue`,
+    initial: true
   })
-  if (!hasBuilt) await buildSelectedPackages()
-  return
+  if (!confirmBuild) {
+    msg.error('Build aborted. 👋')
+    return
+  }
+
+  for (const [i, pkg] of toBePublished.entries()) {
+    await prePublishPackage(pkg, i)
+  }
+
+  console.log(prePublished)
 }
 
 /**
- * Runs builds when given an array of package names
+* Guided process for setting a new version on a package
+*/
+async function prePublishPackage (pkg, index) {
+  const packageJSON = getPackageJSON(pkg)
+  const commitNumber = 5
+  const relevantCommits = getLatestPackageCommits(pkg, commitNumber)
+
+  msg.headline(`\n 🔧  Configuring ${pkg}...`)
+
+  if (index > 0) {
+    // check for dependencies in published object and bump version(s)
+    updatePublishedDependencies(pkg)
+  }
+
+  msg.info(`Latest ${commitNumber} commits affecting files in /packages/${pkg} directory: `)
+  console.log(relevantCommits)
+  console.log(`The package ${chalk.cyan(pkg)} is currently on version ${chalk.cyan(packageJSON.version)}\n`)
+
+  await setNewPackageVersion(pkg)
+}
+
+/**
+ * Checks if a package has dependency versions that need to be bumped
+ * as part of the publish process
  */
-async function buildSelectedPackages (packages = []) {
-  if (!packages.length) {
-    const packageList = getPackages()
-    const { packagesToBuild } = await prompts({
-      type: 'multiselect',
-      name: 'packagesToBuild',
-      message: 'Choose package(s) to build before publishing:',
-      choices: packageList.map(name => ({
-        title: name,
-        value: name
-      })),
-      instructions: false
+function updatePublishedDependencies (pkg) {
+  msg.info(`Checking if ${pkg} has dependencies in need of updating...`)
+  const packageJSON = getPackageJSON(pkg)
+  const dependencies = packageJSON.dependencies ? getFKDependenciesFromObj(packageJSON.dependencies) : []
+
+  for (const dep of Object.keys(prePublished)) {
+    if (checkDependsOn(pkg, dep)) {
+      console.log(chalk.cyan(`Package ${chalk.magenta(pkg)} has a dependency on ${chalk.magenta(dep)}
+Dependency ${chalk.magenta(dep)} will be updated from ${chalk.magenta(prePublished[dep].oldVersion)} to ${chalk.magenta(prePublished[dep].newVersion)}`))
+      const targetNewDepGroup = dependencies.includes(dep) ? 'newDependencies' : 'newDevDependencies'
+      const targetOldDepGroup = dependencies.includes(dep) ? 'oldDependencies' : 'oldDevDependencies'
+      const depName = `@formkit/${dep}`
+      const newDepPayload = { [depName]: prePublished[dep].newVersion }
+      const oldDepPayload = { [depName]: prePublished[dep].oldVersion }
+      prePublished[pkg] = Object.assign({}, prePublished[pkg])
+      // assign new dependencies
+      prePublished[pkg][targetNewDepGroup] = prePublished[pkg][targetNewDepGroup] ?
+        prePublished[pkg][targetNewDepGroup] = Object.assign({}, prePublished[pkg][targetNewDepGroup], newDepPayload) :
+        prePublished[pkg][targetNewDepGroup] = newDepPayload
+      // store old dependencies
+      prePublished[pkg][targetOldDepGroup] = prePublished[pkg][targetOldDepGroup] ?
+        prePublished[pkg][targetOldDepGroup] = Object.assign({}, prePublished[pkg][targetOldDepGroup], oldDepPayload) :
+        prePublished[pkg][targetOldDepGroup] = oldDepPayload
+    }
+  }
+  console.log('\n')
+}
+
+/**
+ * Guided process to set a new package version
+ */
+async function setNewPackageVersion (pkg) {
+  const packageJSON = getPackageJSON(pkg)
+  const isAlphaNumericVer = isAlphaNumericVersion(packageJSON.version)
+  let versionBumpType = 'patch'
+  const versionTypes = ['patch', 'minor', 'major']
+
+  if (!isAlphaNumericVer) {
+    const { bumpType } = await prompts({
+      type: 'select',
+      name: 'bumpType',
+      message: `What type of version update is this publish?`,
+      choices: versionTypes.map((type) => {
+        return {
+          title: type,
+          value: type
+        }
+      })
     })
-    packages = packagesToBuild
+    versionBumpType = bumpType
+  } else {
+    versionBumpType = 'alphaNumeric'
   }
-  if (packages.length) {
-    await buildAllPackages(packages)
+
+  const { newPackageVersion } = await prompts({
+    type: 'text',
+    name: 'newPackageVersion',
+    message: `What should the new version be?`,
+    initial: suggestVersionIncrement(packageJSON.version, versionBumpType)
+  })
+
+  prePublished[pkg] = Object.assign({}, prePublished[pkg], {
+    oldVersion: packageJSON.version,
+    newVersion: newPackageVersion
+  })
+}
+
+/**
+ * Given a version string and a version bump type - suggest the next version
+ */
+function suggestVersionIncrement(version, updateType) {
+  let versionParts = version.split('.')
+  let targetIndex = versionParts.length - 1
+  switch (updateType) {
+    case 'major':
+      targetIndex = 0
+      break
+    case 'minor':
+      targetIndex = 1
+      break
+    default:
+      targetIndex = versionParts.length - 1
+      break
+  }
+
+  versionParts[targetIndex]++
+  return versionParts.join('.')
+}
+
+/**
+ * Confirm with user that all packages are going to be built
+ */
+async function buildAllPackagesConsent () {
+  const { shouldBuild } = await prompts({
+    type: 'confirm',
+    name: 'shouldBuild',
+    message: `Publishing will generate clean builds of ${chalk.red('ALL')} packages. Continue?`,
+    initial: true
+  })
+  return shouldBuild
+}
+
+/**
+ * Checks each package to see if the dist directory is different
+ * than the currently published NPM module
+ */
+async function getChangedDist () {
+  for (const p of allPackages) {
+    // TODO: compare against NPM published data
+    // for now just say that only core is unchanged.
+    // When NPM is available, only compare .esm.js and .d.ts files
+    if (p === 'utils') {
+      toBePublished.push(p)
+    }
   }
 }
 
 /**
- * Get a list of packages to publish from the user
+ * Given a list of packages, determine the correct order to publish them
+ * so that upgraded dependencies become included within their dependents
  */
-async function getPackagesToPublish () {
-  const packageList = getPackages()
-  const { packagesToPublish } = await prompts({
-    type: 'multiselect',
-    name: 'packagesToPublish',
-    message: 'Which package(s) would you like to publish:',
-    choices: packageList.map(name => ({
-      title: name,
-      value: name
-    })),
-    instructions: false
-  })
-  return packagesToPublish
+async function getPublishOrder (packages) {
+  const dependentsTree = await getDependencyTree(packages, true)
+  return await flattenDependencyTree(dependentsTree)
 }
 
 /**
--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 import fs from 'fs'
+import { execSync } from "child_process"
 import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 import ora from 'ora'
@@ -15,7 +16,13 @@ export const msg = {
   info: m => console.log(chalk.cyan(m)),
   success: m => console.log(chalk.green(m)),
   label: m => console.log(chalk.bold.magenta(m)),
+  headline: m => console.log(chalk.bold.magenta(`\n\n${'='.repeat(Math.min(80, m.length))}${m}\n${'='.repeat(Math.min(80, m.length))}\n`)),
   loader: ora()
+}
+
+/** Given a version string, is it alphanumeric? */
+export function isAlphaNumericVersion (string) {
+  return /[a-z].(\d+)$/.test(string)
 }
 
 /**
@@ -32,21 +39,22 @@ export function getPackages() {
 export function getBuildOrder(packages = [], orderedPackages = []) {
   const packagesSet = new Set(packages)
   let orderedPackagesSet = new Set(orderedPackages)
-  for (const p of packages) {
-    packagesSet.delete(p)
-    const dependencies = getPackageDependencies(p)
-    for (const d of dependencies) {
-      if (!orderedPackagesSet.has(d)) {
-        const nestedDeps = getBuildOrder([d], [...orderedPackagesSet])
-        orderedPackagesSet = new Set([...nestedDeps, d])
+  for (const pkg of packages) {
+    packagesSet.delete(pkg)
+    const dependencies = getPackageDependencies(pkg)
+    for (const dep of dependencies) {
+      if (!orderedPackagesSet.has(dep)) {
+        const nestedDeps = getBuildOrder([dep], [...orderedPackagesSet])
+        orderedPackagesSet = new Set([...nestedDeps, dep])
       }
-      if (!orderedPackagesSet.has(p)) {
-        orderedPackagesSet = new Set([...orderedPackagesSet, p])
+      if (!orderedPackagesSet.has(pkg)) {
+        orderedPackagesSet = new Set([...orderedPackagesSet, pkg])
       }
     }
     if (packagesSet && packages.size) {
       return getBuildOrder(packages, orderedPackages)
     }
+    orderedPackagesSet.add(pkg)
   }
   return [...orderedPackagesSet]
 }
@@ -58,60 +66,137 @@ export function getBuildOrder(packages = [], orderedPackages = []) {
 export async function getDependencyTree (packages, inverse = false) {
   const tree = []
   const allPackages = await getPackages()
-  for (const p of packages) {
+  for (const pkg of packages) {
     const dependencies = []
     for (const ap of allPackages) {
-      if (inverse ? checkDependsOn(p, ap) : checkDependsOn(ap, p)) {
+      if (inverse ? checkDependsOn(ap, pkg) : checkDependsOn(pkg, ap)) {
         const subDependencies = await getDependencyTree([ap], tree, inverse)
         dependencies.push(...subDependencies)
       }
     }
-    tree.push([p, dependencies.length ? dependencies : false])
+    tree.push([pkg, dependencies.length ? dependencies : false])
   }
   return tree
 }
 
 /**
- * Given a package and a dependency, see if the given dependency is a
- * dependent of the package
+ * Given a dependency tree, produce a flat array of package names in order
+ * of dependency
+*/
+export async function flattenDependencyTree (tree = [], orderedList = new Set()) {
+  const buildOrder = getBuildOrder(await getPackages())
+  if (!Array.isArray(tree)) return []
+  for (const branch of tree) {
+    const pkg = branch[0]
+    const subtree = branch[1]
+
+    if (!orderedList.has(pkg)) {
+      orderedList = new Set([...orderedList, pkg, ...await flattenDependencyTree(subtree)])
+    }
+  }
+  orderedList = [...orderedList]
+  orderedList.sort(function(a, b){
+    return buildOrder.indexOf(a) - buildOrder.indexOf(b);
+  })
+  return orderedList
+}
+
+/**
+ * Given a package and a dependency, see if the given package includes
+ * the given dependency
  */
-export function checkDependsOn (p, dependency) {
-  const allDeps = getPackageDependencies(dependency)
-  return allDeps.includes(p)
+export function checkDependsOn (pkg, dependency) {
+  const allDeps = getPackageDependencies(pkg)
+  return allDeps.includes(dependency)
 }
 
 
 /**
  * Given a dependency tree, do a pretty console log of the graph
  */
-export function drawDependencyTree (tree = [], depth = 0, directoryPrefix = '∟ ') {
+export function drawDependencyTree (tree = [], depth = 0, directoryPrefix = '∟ ', parent) {
   for (const [i, branch] of tree.entries()) {
     const title = branch[0]
     const deps = branch[1]
     const directoryIndent = `${'  '.repeat(depth)}${depth > 0 ? directoryPrefix : ''}${'— '.repeat(Math.min(1, depth))}`
     if (depth === 0) {
-      console.log(`${i + 1}) ${directoryIndent}${title}`)
+      console.log(`${i + 1}) ${directoryIndent}${title} ` + chalk.dim(`(${getPackageVersion(title)})`))
     } else {
-      msg.info(`  ${directoryIndent}${title}`)
+      msg.info(`  ${directoryIndent}${title} ` + chalk.dim(`(${getDependencyVersion(parent, title)})`))
     }
 
     if (deps) {
       for (const dep of deps) {
-        drawDependencyTree([dep], depth + 1, '∟ ')
+        drawDependencyTree([dep], depth + 1, directoryPrefix, title)
       }
     }
   }
 }
 
 /**
+* Given a package name, return relevant build files from the file system
+ */
+export function getPackageFromFS (pkg) {
+  const packageJSON = getPackageJSON(pkg)
+  const packageESM = getRawBuildFile(pkg, packageJSON.module)
+  const packageTS = getRawBuildFile(pkg, packageJSON.types)
+
+  return {
+    packageJSON,
+    packageESM,
+    packageTS
+  }
+}
+
+
+/**
+* Given a package name, return relevant build files from the file system
+ */
+export function getRawBuildFile(pkg, path) {
+  return fs.readFileSync(`${packagesDir}/${pkg}/${path}`, 'utf-8')
+}
+
+/**
+ * Given a package name, retrieve the package.json data
+ */
+export function getPackageJSON (pkg) {
+  const packageJSONRaw = fs.readFileSync(`${packagesDir}/${pkg}/package.json`);
+  return JSON.parse(packageJSONRaw)
+}
+
+/**
  * Provide array of dependency packages for a provided package
  */
-export function getPackageDependencies (p) {
-  const packageJSONRaw = fs.readFileSync(resolve(__dirname, `../packages/${p}/package.json`));
-  const packageJSON = JSON.parse(packageJSONRaw)
+export function getPackageDependencies (pkg) {
+  const packageJSON = getPackageJSON(pkg)
   const dependencies = packageJSON.dependencies ? packageJSON.dependencies : []
   const devDependencies = packageJSON.devDependencies ? packageJSON.devDependencies : []
   return [...getFKDependenciesFromObj(dependencies), ...getFKDependenciesFromObj(devDependencies)]
+}
+
+/**
+ * Given a package name get the current version frem the package.json
+ */
+export function getPackageVersion (pkg) {
+  const packageJSON = getPackageJSON(pkg)
+  return packageJSON.version
+}
+
+/**
+ * Given a dependency and parent package get the current version of the dependency
+ * from the parent the package.json
+ */
+export function getDependencyVersion (pkg, parent) {
+  const packageJSON = getPackageJSON(parent)
+  const dependencies = packageJSON.dependencies ? packageJSON.dependencies : []
+  const devDependencies = packageJSON.devDependencies ? packageJSON.devDependencies : []
+  if (Object.keys(dependencies).includes(`@formkit/${pkg}`)) {
+    return dependencies[`@formkit/${pkg}`]
+  }
+  if (Object.keys(devDependencies).includes(`@formkit/${pkg}`)) {
+    return devDependencies[`@formkit/${pkg}`]
+  }
+  return 'null'
 }
 
 /**
@@ -121,4 +206,29 @@ export function getFKDependenciesFromObj(dependencies) {
   let matches = Object.keys(dependencies).filter((key) => key.startsWith('@formkit/'))
   matches = matches.map(dependency => dependency.replace('@formkit/', ''))
   return matches
+}
+
+/**
+ * Given a package name, this will get the latest N commits in the associated directory
+ */
+export function getLatestPackageCommits (pkg, n = 15) {
+  const commitLog = execSync(
+    `git rev-list HEAD --all --reverse --max-count=${n} --pretty=oneline -- "${packagesDir}/${pkg}"`,
+    {
+      encoding: 'utf-8'
+    }
+  )
+  return commitLog
+}
+
+/**
+ * Returns true if the current git working directory is clean
+ */
+export function checkGitCleanWorkingDirectory () {
+  return execSync(
+    `git status --untracked-files=no --porcelain`,
+    {
+      encoding: 'utf-8'
+    }
+  )
 }
