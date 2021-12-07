d8a9126768bc6dd8681729775bb92a0797574d07
Justin Schroeder <justin@formkit.com>
2021-12-06 21:17:16 -0500

    Adds a defaultConfig test for plugins

--- packages/vue/__tests__/defaultConfig.spec.ts
+++ packages/vue/__tests__/defaultConfig.spec.ts
@@ -3,7 +3,8 @@ import { plugin } from '../src/plugin'
 import defaultConfig from '../src/defaultConfig'
 import { mount } from '@vue/test-utils'
 import { FormKitValidationRule } from '@formkit/validation'
-import { getNode } from '@formkit/core'
+import { getNode, FormKitPlugin } from '@formkit/core'
+import { jest } from '@jest/globals'
 
 describe('defaultConfig', () => {
   it('allows rule augmentation', async () => {
@@ -23,5 +24,23 @@ describe('defaultConfig', () => {
     node.input('monday', false)
     await new Promise((r) => setTimeout(r, 5))
     expect(node.store).not.toHaveProperty('rule_monday')
+  })
+
+  it('allows plugin augmentation', async () => {
+    const newPlugin: FormKitPlugin = jest.fn((node) =>
+      node.hook.input((value) => value + '.')
+    )
+    mount(FormKit, {
+      props: {
+        id: 'tuesday',
+        value: 'foobar',
+      },
+      global: {
+        plugins: [[plugin, defaultConfig({ plugins: [newPlugin] })]],
+      },
+    })
+    expect(newPlugin).toHaveBeenCalledTimes(1)
+    const node = getNode('tuesday')!
+    expect(node.value).toBe('foobar.')
   })
 })
