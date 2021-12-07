efc094cdfe008fc1c984d46a37dbb4d99b43683b
Justin Schroeder <justin@formkit.com>
2021-12-07 12:12:18 -0500

    Adds german locale, thank you @digitalkaoz. Closes #25

---
+++ packages/i18n/src/locales/de.ts
@@ -0,0 +1,306 @@
+import { FormKitValidationMessages } from '@formkit/validation'
+
+/**
+ * Here we can import additional helper functions to assist in formatting our
+ * language. Feel free to add additional helper methods to libs/formats if it
+ * assists in creating good validation messages for your locale.
+ */
+import { sentence as s, list, date } from '../formatters'
+import { FormKitLocaleMessages } from '../i18n'
+
+/**
+ * Standard language for interface features.
+ * @public
+ */
+export const ui: FormKitLocaleMessages = {
+  /**
+   * Shown when a button to remove items is visible.
+   */
+  remove: 'Entfernen',
+  /**
+   * Shown when all fields are not filled out correctly.
+   */
+  incomplete: 'Einige Felder enthalten Fehler.',
+  /**
+   * Shown in a button inside a form to submit the form.
+   */
+  submit: 'Senden',
+}
+
+/**
+ * These are all the possible strings that pertain to validation messages.
+ * @public
+ */
+export const validation: FormKitValidationMessages = {
+  /**
+   * The value is not an accepted value.
+   * @see {@link https://docs.formkit.com/essentials/validation#accepted}
+   */
+  accepted({ name }): string {
+    /* <i18n case="Shown when the user-provided value is not a valid 'accepted' value."> */
+    return `Bitte akzeptiere ${name}.`
+    /* </i18n> */
+  },
+
+  /**
+   * The date is not after
+   * @see {@link https://docs.formkit.com/essentials/validation#date-after}
+   */
+  date_after({ name, args }) {
+    if (Array.isArray(args) && args.length) {
+      /* <i18n case="Shown when the user-provided date is not after the date supplied to the rule."> */
+      return `${s(name)} muss nach dem ${date(args[0])} liegen.`
+      /* </i18n> */
+    }
+    /* <i18n case="Shown when the user-provided date is not after today's date, since no date was supplied to the rule."> */
+    return `${s(name)} muss in der Zukunft liegen.`
+    /* </i18n> */
+  },
+
+  /**
+   * The value is not a letter.
+   * @see {@link https://docs.formkit.com/essentials/validation#alpha}
+   */
+  alpha({ name }) {
+    /* <i18n case="Shown when the user-provided value contains non-alphabetical characters."> */
+    return `${s(name)} kann nur Buchstaben enthalten.`
+    /* </i18n> */
+  },
+
+  /**
+   * The value is not alphanumeric
+   * @see {@link https://docs.formkit.com/essentials/validation#alphanumeric}
+   */
+  alphanumeric({ name }) {
+    /* <i18n case="Shown when the user-provided value contains non-alphanumeric characters."> */
+    return `${s(name)} kann nur Zahlen und Buchstaben enthalten.`
+    /* </i18n> */
+  },
+
+  /**
+   * The date is not before
+   * @see {@link https://docs.formkit.com/essentials/validation#date-before}
+   */
+  date_before({ name, args }) {
+    if (Array.isArray(args) && args.length) {
+      /* <i18n case="Shown when the user-provided date is not before the date supplied to the rule."> */
+      return `${s(name)} muss vor dem ${date(args[0])} liegen.`
+      /* </i18n> */
+    }
+    /* <i18n case="Shown when the user-provided date is not before today's date, since no date was supplied to the rule."> */
+    return `${s(name)} muss in der Vergangenheit liegen.`
+    /* </i18n> */
+  },
+
+  /**
+   * The value is not between two numbers
+   * @see {@link https://docs.formkit.com/essentials/validation#between}
+   */
+  between({ name, args }) {
+    if (isNaN(args[0]) || isNaN(args[1])) {
+      /* <i18n case="Shown when any of the arguments supplied to the rule were not a number."> */
+      return `Dieses Feld ist fehlerhaft konfiguriert.`
+      /* </i18n> */
+    }
+    /* <i18n case="Shown when the user-provided value is not between two numbers."> */
+    return `${s(name)} muss zwischen ${args[0]} und ${args[1]} liegen.`
+    /* </i18n> */
+  },
+
+  /**
+   * The confirmation field does not match
+   * @see {@link https://docs.formkit.com/essentials/validation#confirm}
+   */
+  confirm({ name }) {
+    /* <i18n case="Shown when the user-provided value does not equal the value of the matched input."> */
+    return `${s(name)} stimmt nicht überein.`
+    /* </i18n> */
+  },
+
+  /**
+   * The value is not a valid date
+   * @see {@link https://docs.formkit.com/essentials/validation#date-format}
+   */
+  date_format({ name, args }) {
+    if (Array.isArray(args) && args.length) {
+      /* <i18n case="Shown when the user-provided date does not satisfy the date format supplied to the rule."> */
+      return `${s(name)} ist kein gültiges Datum, bitte nutze das Format ${
+        args[0]
+      }`
+      /* </i18n> */
+    }
+    /* <i18n case="Shown when no date argument was supplied to the rule."> */
+    return 'Dieses Feld enthält einen ungültigen Wert.'
+    /* </i18n> */
+  },
+
+  /**
+   * Is not within expected date range
+   * @see {@link https://docs.formkit.com/essentials/validation#date-between}
+   */
+  date_between({ name, args }) {
+    /* <i18n case="Shown when the user-provided date is not between the start and end dates supplied to the rule. "> */
+    return `${s(name)} muss zwischen dem ${date(args[0])} und dem ${date(
+      args[1]
+    )} liegen.`
+    /* </i18n> */
+  },
+
+  /**
+   * Shown when the user-provided value is not a valid email address.
+   * @see {@link https://docs.formkit.com/essentials/validation#email}
+   */
+  email: 'Dies ist keine gültige E-Mail.',
+
+  /**
+   * Does not end with the specified value
+   * @see {@link https://docs.formkit.com/essentials/validation#ends-with}
+   */
+  ends_with({ name, args }) {
+    /* <i18n case="Shown when the user-provided value does not end with the substring supplied to the rule."> */
+    return `${s(name)} endet nicht mit ${list(args)}.`
+    /* </i18n> */
+  },
+
+  /**
+   * Is not an allowed value
+   * @see {@link https://docs.formkit.com/essentials/validation#is}
+   */
+  is({ name }) {
+    /* <i18n case="Shown when the user-provided value is not one of the values supplied to the rule."> */
+    return `${s(name)} ist hier nicht erlaubt.`
+    /* </i18n> */
+  },
+
+  /**
+   * Does not match specified length
+   * @see {@link https://docs.formkit.com/essentials/validation#length}
+   */
+  length({ name, args: [first = 0, second = Infinity] }) {
+    const min = first <= second ? first : second
+    const max = second >= first ? second : first
+    if (min == 1 && max === Infinity) {
+      /* <i18n case="Shown when the length of the user-provided value is not at least one character."> */
+      return `${s(name)} muss mindestens ein Zeichen enthalten.`
+      /* </i18n> */
+    }
+    if (min == 0 && max) {
+      /* <i18n case="Shown when first argument supplied to the rule is 0, and the user-provided value is longer than the max (the 2nd argument) supplied to the rule."> */
+      return `${s(name)} darf höchstens ${max} Zeichen enthalten.`
+      /* </i18n> */
+    }
+    if (min && max === Infinity) {
+      /* <i18n case="Shown when the length of the user-provided value is less than the minimum supplied to the rule and there is no maximum supplied to the rule."> */
+      return `${s(name)} muss mindestens ${min} Zeichen enthalten.`
+      /* </i18n> */
+    }
+    /* <i18n case="Shown when the length of the user-provided value is between the two lengths supplied to the rule."> */
+    return `${s(name)} muss zwischen ${min} und ${max} Zeichen enthalten.`
+    /* </i18n> */
+  },
+
+  /**
+   * Value is not a match
+   * @see {@link https://docs.formkit.com/essentials/validation#matches}
+   */
+  matches({ name }) {
+    /* <i18n case="Shown when the user-provided value does not match any of the values or RegExp patterns supplied to the rule. "> */
+    return `${s(name)} ist hier nicht erlaubt.`
+    /* </i18n> */
+  },
+
+  /**
+   * Exceeds maximum allowed value
+   * @see {@link https://docs.formkit.com/essentials/validation#max}
+   */
+  max({ name, node: { value }, args }) {
+    if (Array.isArray(value)) {
+      /* <i18n case="Shown when the length of the array of user-provided values is longer than the max supplied to the rule."> */
+      return `${name} kann nicht mehr als ${args[0]} Elemente enhalten.`
+      /* </i18n> */
+    }
+    /* <i18n case="Shown when the user-provided value is greater than the maximum number supplied to the rule."> */
+    return `${s(name)} darf höchstens ${args[0]} sein.`
+    /* </i18n> */
+  },
+
+  /**
+   * The (field-level) value does not match specified mime type
+   * @see {@link https://docs.formkit.com/essentials/validation#mime}
+   */
+  mime({ name, args }) {
+    if (!args[0]) {
+      /* <i18n case="Shown when no file formats were supplied to the rule."> */
+      return 'Keine Dateitypen erlaubt.'
+      /* </i18n> */
+    }
+    /* <i18n case="Shown when the mime type of user-provided file does not match any mime types supplied to the rule."> */
+    return `${s(name)} muss vom Dateityp ${args[0]} sein.`
+    /* </i18n> */
+  },
+
+  /**
+   * Does not fulfill minimum allowed value
+   * @see {@link https://docs.formkit.com/essentials/validation#min}
+   */
+  min({ name, node: { value }, args }) {
+    if (Array.isArray(value)) {
+      /* <i18n case="Shown when the length of the array of user-provided values is shorter than the min supplied to the rule."> */
+      return `${name} muss mindestens ${args[0]} Elemente enthalten.`
+      /* </i18n> */
+    }
+    /* <i18n case="Shown when the user-provided value is less than the minimum number supplied to the rule."> */
+    return `${s(name)} muss mindestens ${args[0]} sein.`
+    /* </i18n> */
+  },
+
+  /**
+   * Is not an allowed value
+   * @see {@link https://docs.formkit.com/essentials/validation#not}
+   */
+  not({ name, node: { value } }) {
+    /* <i18n case="Shown when the user-provided value matches one of the values supplied to (and thus disallowed by) the rule."> */
+    return `"${value}" ist nicht erlaubt für ${name}.`
+    /* </i18n> */
+  },
+
+  /**
+   *  Is not a number
+   * @see {@link https://docs.formkit.com/essentials/validation#number}
+   */
+  number({ name }) {
+    /* <i18n case="Shown when the user-provided value is not a number."> */
+    return `${s(name)} muss eine Zahl enthalten.`
+    /* </i18n> */
+  },
+
+  /**
+   * Required field.
+   * @see {@link https://docs.formkit.com/essentials/validation#required}
+   */
+  required({ name }) {
+    /* <i18n case="Shown when a user does not provide a value to a required input."> */
+    return `${s(name)} ist ein Pflichtfeld.`
+    /* </i18n> */
+  },
+
+  /**
+   * Does not start with specified value
+   * @see {@link https://docs.formkit.com/essentials/validation#starts-with}
+   */
+  starts_with({ name, args }) {
+    /* <i18n case="Shown when the user-provided value does not start with the substring supplied to the rule."> */
+    return `${s(name)} beginnt nicht mit ${list(args)}.`
+    /* </i18n> */
+  },
+
+  /**
+   * Is not a url
+   * @see {@link https://docs.formkit.com/essentials/validation#url}
+   */
+  url() {
+    /* <i18n case="Shown when the user-provided value is not a valid url."> */
+    return `Dies ist keine gültige URL.`
+    /* </i18n> */
+  },
+}
