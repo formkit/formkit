24ce212a76107710043646d4eb1e1bb821749bf3
Justin Schroeder <justin@formkit.com>
2022-02-15 16:02:43 -0500

    Adds empty string as initial value when not defined

--- packages/inputs/src/features/initialValue.ts
+++ packages/inputs/src/features/initialValue.ts
@@ -3,7 +3,7 @@ import { FormKitNode } from '@formkit/core'
 export default function initialValue(node: FormKitNode): void {
   node.on('created', () => {
     if (node.context) {
-      node.context.initialValue = node.value
+      node.context.initialValue = node.value || ''
     }
   })
 }
