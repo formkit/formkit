3d6a004f74066fa582f8c10c0f3b2d0f2aab301d
Dominik Klein <dk@zammad.com>
2022-02-16 15:53:18 +0100

    Adds missing type exports which are marked as public.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -24,7 +24,11 @@ export {
 /**
  * The FormKit ledger.
  */
-export { FormKitLedger } from './ledger'
+export {
+  FormKitLedger,
+  FormKitCounterCondition,
+  FormKitCounter
+} from './ledger'
 
 /**
  * Export dispatcher typings.
@@ -68,6 +72,7 @@ export {
   FormKitSchemaDOMNode,
   FormKitSchemaFormKit,
   FormKitSchemaNode,
+  FormKitSchemaProps,
   FormKitSchemaTextNode,
   isComponent,
   isConditional,
