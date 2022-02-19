854ce98cb82b8e2184112d92625cde160db67aba
Justin Schroeder <justin@formkit.com>
2022-02-18 22:55:26 -0500

    Adds spanish language, thanks @inibg

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,14 +1,15 @@
 import * as de from './locales/de'
 import * as en from './locales/en'
+import * as es from './locales/es'
 import * as fa from './locales/fa'
 import * as fi from './locales/fi'
 import * as fr from './locales/fr'
 import * as he from './locales/he'
-import * as tr from './locales/tr'
 import * as hr from './locales/hr'
 import * as nl from './locales/nl'
 import * as pt from './locales/pt'
 import * as ru from './locales/ru'
+import * as tr from './locales/tr'
 import * as zh from './locales/zh'
 
 /**
@@ -23,6 +24,7 @@ export * from './i18n'
 export const locales = {
   de,
   en,
+  es,
   fa,
   fi,
   fr,
@@ -38,7 +40,7 @@ export const locales = {
 /**
  * Export each locale individually for people who want to cherry pick.
  */
-export { de, en, fa, fi, fr, he, hr, nl, pt, ru, tr, zh }
+export { de, en, es, fa, fi, fr, he, hr, nl, pt, ru, tr, zh }
 
 /**
  * Export all formatter functions.
