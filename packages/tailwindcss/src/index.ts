503831ddae58a33e9e35ae84b89581bb905d5f4b
Andrew Boyd <andrew@wearebraid.com>
2022-03-04 16:52:01 -0500

    updates selectors to account for all permutations
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -1,12 +1,12 @@
 import plugin from 'tailwindcss/plugin'
 
 const formKitStates = plugin(({ addVariant }) => {
-  addVariant('formkit-disabled', ['[data-type][data-disabled]', '[data-type][data-disabled] &'])
-  addVariant('formkit-invalid', ['[data-type][data-invalid]', '[data-type][data-invalid] &'])
-  addVariant('formkit-errors', ['[data-type][data-errors]', '[data-type][data-errors] &'])
-  addVariant('formkit-complete', ['[data-type][data-complete]', '[data-type][data-complete] &'])
-  addVariant('formkit-loading', ['[data-type][data-loading]', '[data-type][data-loading] &'])
-  addVariant('formkit-submitted', ['[data-type][data-submitted]', '[data-type][data-submitted] &'])
-  addVariant('formkit-multiple', ['[data-type][data-multiple]', '[data-type][data-multiple] &'])
+  addVariant('formkit-disabled', ['&[data-disabled]', '[data-disabled] &', '[data-disabled]&'])
+  addVariant('formkit-invalid', ['&[data-invalid]', '[data-invalid] &', '[data-invalid]&'])
+  addVariant('formkit-errors', ['&[data-errors]', '[data-errors] &', '[data-errors]&'])
+  addVariant('formkit-complete', ['&[data-complete]', '[data-complete] &', '[data-complete]&'])
+  addVariant('formkit-loading', ['&[data-loading]', '[data-loading] &', '[data-loading]&'])
+  addVariant('formkit-submitted', ['&[data-submitted]', '[data-submitted] &', '[data-submitted]&'])
+  addVariant('formkit-multiple', ['&[data-multiple]', '[data-multiple] &', '[data-multiple]&'])
 })
 export default formKitStates
