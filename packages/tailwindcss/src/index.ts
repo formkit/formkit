1f4902d9ee24cd6409e5fe519d320bc9bdeda8dc
Andrew Boyd <andrew@wearebraid.com>
2022-03-04 15:56:52 -0500

    adds variants for new FormKit data attributes
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -1,10 +1,12 @@
 import plugin from 'tailwindcss/plugin'
 
 const formKitStates = plugin(({ addVariant }) => {
+  addVariant('formkit-disabled', ['[data-type][data-disabled]', '[data-type][data-disabled] &'])
   addVariant('formkit-invalid', ['[data-type][data-invalid]', '[data-type][data-invalid] &'])
   addVariant('formkit-errors', ['[data-type][data-errors]', '[data-type][data-errors] &'])
-  addVariant('formkit-disabled', ['[data-type][data-disabled]', '[data-type][data-disabled] &'])
   addVariant('formkit-complete', ['[data-type][data-complete]', '[data-type][data-complete] &'])
+  addVariant('formkit-loading', ['[data-type][data-loading]', '[data-type][data-loading] &'])
+  addVariant('formkit-submitted', ['[data-type][data-submitted]', '[data-type][data-submitted] &'])
   addVariant('formkit-multiple', ['[data-type][data-multiple]', '[data-type][data-multiple] &'])
 })
 export default formKitStates
