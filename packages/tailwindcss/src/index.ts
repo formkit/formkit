884e83e0b116af07cf844b319a75b706af25518c
Andrew Boyd <andrew@wearebraid.com>
2022-03-05 11:02:44 -0500

    adds draft of `generateClasses` function to create tailwind theme from POJO
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -1,6 +1,9 @@
+import { FormKitNode, FormKitClasses } from '@formkit/core'
 import plugin from 'tailwindcss/plugin'
 
-const formKitStates = plugin(({ addVariant }) => {
+// FormKit plugin for Tailwind
+// used in tailwind.config.js
+const formKitVariants = plugin(({ addVariant }) => {
   addVariant('formkit-disabled', ['&[data-disabled]', '[data-disabled] &', '[data-disabled]&'])
   addVariant('formkit-invalid', ['&[data-invalid]', '[data-invalid] &', '[data-invalid]&'])
   addVariant('formkit-errors', ['&[data-errors]', '[data-errors] &', '[data-errors]&'])
@@ -9,4 +12,66 @@ const formKitStates = plugin(({ addVariant }) => {
   addVariant('formkit-submitted', ['&[data-submitted]', '[data-submitted] &', '[data-submitted]&'])
   addVariant('formkit-multiple', ['&[data-multiple]', '[data-multiple] &', '[data-multiple]&'])
 })
-export default formKitStates
+
+// Helper function for Tailwind styling.
+// to be imported and used in FormKit config
+export function generateClasses (
+  classes:Record<string, Record<string, string>>
+): any {
+  const classesBySectionKey:Record<string, Record<string, any>> = {}
+  Object.keys(classes).forEach(type => {
+    Object.keys(classes[type]).forEach(sectionKey => {
+      if (!classesBySectionKey[sectionKey]) {
+        classesBySectionKey[sectionKey] = {
+          [type]: classes[type][sectionKey]
+        }
+      } else {
+        classesBySectionKey[sectionKey][type] = classes[type][sectionKey]
+      }
+    })
+  })
+
+  Object.keys(classesBySectionKey).forEach(sectionKey => {
+    const classesObject = classesBySectionKey[sectionKey]
+    classesBySectionKey[sectionKey] = function(
+      node: FormKitNode,
+      sectionKey: string,
+      sectionClassList: FormKitClasses | string | Record<string, boolean>,
+      classesByType: any = classesObject
+    ) {
+      return formKitStates(node, sectionKey, sectionClassList, classesByType)
+    }
+  })
+
+  return classesBySectionKey
+}
+
+function formKitStates (
+  node: FormKitNode,
+  sectionKey: string,
+  sectionClassList: FormKitClasses | string | Record<string, boolean>,
+  classesByType: any
+): string {
+  if (!sectionClassList) {
+    // just doing this because typescript won't let me not
+    // touch the argument. Need advice
+    sectionClassList = ''
+  }
+  if (!sectionKey) {
+    // just doing this because typescript won't let me not
+    // touch the argument. Need advice
+    sectionKey = ''
+  }
+
+  const type = node.props.type
+  let classList = ''
+  if (classesByType.global) {
+    classList += classesByType.global + ' '
+  }
+  if (classesByType[type]) {
+    classList += classesByType[type]
+  }
+  return classList.trim()
+}
+
+export default formKitVariants
