8a81061334ad2491051730e9331025414b3725a7
Andrew Boyd <andrew@wearebraid.com>
2022-03-07 11:34:44 -0500

    improves types for Tailwind package
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -1,9 +1,12 @@
 import { FormKitNode, FormKitClasses } from '@formkit/core'
 import plugin from 'tailwindcss/plugin'
 
-// FormKit plugin for Tailwind
-// used in tailwind.config.js
+/**
+ * The FormKit plugin for Tailwind
+ * @public
+ */
 const formKitVariants = plugin(({ addVariant }) => {
+  console.log('plugin is running')
   addVariant('formkit-disabled', ['&[data-disabled]', '[data-disabled] &', '[data-disabled]&'])
   addVariant('formkit-invalid', ['&[data-invalid]', '[data-invalid] &', '[data-invalid]&'])
   addVariant('formkit-errors', ['&[data-errors]', '[data-errors] &', '[data-errors]&'])
@@ -11,13 +14,35 @@ const formKitVariants = plugin(({ addVariant }) => {
   addVariant('formkit-loading', ['&[data-loading]', '[data-loading] &', '[data-loading]&'])
   addVariant('formkit-submitted', ['&[data-submitted]', '[data-submitted] &', '[data-submitted]&'])
   addVariant('formkit-multiple', ['&[data-multiple]', '[data-multiple] &', '[data-multiple]&'])
+  addVariant('formkit-action', ['.formkit-actions &', '.formkit-actions&'])
 })
 
-// Helper function for Tailwind styling.
-// to be imported and used in FormKit config
+/**
+ * An object of ClassFunctions
+ * @internal
+ */
+interface FormKitClassFunctions {
+  [index:string]: ClassFunction
+}
+
+/**
+ * A function that returns a class list string
+ * @internal
+ */
+type ClassFunction = (
+  node: FormKitNode,
+  sectionKey: string,
+  sectionClassList: FormKitClasses | string | Record<string, boolean>
+) => string
+
+/**
+ * A function to generate FormKit class functions from a javascript object
+ * @param classes - An object of input types with nested objects of sectionKeys and class lists
+ * @returns FormKitClassFunctions
+ */
 export function generateClasses (
   classes:Record<string, Record<string, string>>
-): any {
+): FormKitClassFunctions {
   const classesBySectionKey:Record<string, Record<string, any>> = {}
   Object.keys(classes).forEach(type => {
     Object.keys(classes[type]).forEach(sectionKey => {
@@ -36,33 +61,21 @@ export function generateClasses (
     classesBySectionKey[sectionKey] = function(
       node: FormKitNode,
       sectionKey: string,
-      sectionClassList: FormKitClasses | string | Record<string, boolean>,
-      classesByType: any = classesObject
+      sectionClassList: FormKitClasses | string | Record<string, boolean>
     ) {
-      return formKitStates(node, sectionKey, sectionClassList, classesByType)
+      return formKitStates(node, sectionKey, sectionClassList, classesObject)
     }
   })
 
-  return classesBySectionKey
+  return classesBySectionKey as FormKitClassFunctions
 }
 
 function formKitStates (
   node: FormKitNode,
-  sectionKey: string,
-  sectionClassList: FormKitClasses | string | Record<string, boolean>,
-  classesByType: any
+  _sectionKey: string,
+  _sectionClassList: FormKitClasses | string | Record<string, boolean>,
+  classesByType: Record<string, () => string>
 ): string {
-  if (!sectionClassList) {
-    // just doing this because typescript won't let me not
-    // touch the argument. Need advice
-    sectionClassList = ''
-  }
-  if (!sectionKey) {
-    // just doing this because typescript won't let me not
-    // touch the argument. Need advice
-    sectionKey = ''
-  }
-
   const type = node.props.type
   let classList = ''
   if (classesByType.global) {
