40e0447dd74016cfa6e637fa92a4d72d469acc2d
Andrew Boyd <andrew@wearebraid.com>
2022-03-07 13:58:00 -0500

    removes extranious markup from Tailwind plugin playground App.vue
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- packages/tailwindcss/playground/src/App.vue
+++ packages/tailwindcss/playground/src/App.vue
@@ -5,53 +5,31 @@
         type="form"
       >
         <FormKit
-          type="group"
-          v-model="formData"
-        >
-          <FormKit
-            label="Invalid input"
-            placeholder="Enter something here..."
-            validation="required"
-            validation-visibility="live"
-          />
-          <FormKit
-            label="Disabled input"
-            :disabled="true"
-            placeholder="Enter something here..."
-          />
-          <FormKit
-            label="Complete input"
-            value="I have a value"
-          />
-          <FormKit
-            label="Complete input"
-            type="select"
-            multiple
-            :options="['option-1', 'option-2']"
-            value="I have a value"
-          />
-        </FormKit>
+          label="Invalid input"
+          placeholder="Enter something here..."
+          validation="required"
+          validation-visibility="live"
+        />
+        <FormKit
+          label="Disabled input"
+          :disabled="true"
+          placeholder="Enter something here..."
+        />
+        <FormKit
+          label="Complete input"
+          value="I have a value"
+        />
+        <FormKit
+          label="Complete input"
+          type="select"
+          multiple
+          :options="['option-1', 'option-2']"
+          value="I have a value"
+        />
       </FormKit>
     </div>
   </div>
 </template>
-
-<script>
-import { ref } from 'vue'
-
-export default {
-  setup() {
-    const recommendation = ref('10')
-    const formData = ref({})
-
-    return {
-      recommendation,
-      formData
-    }
-  }
-}
-</script>
-
 
 <style>
 #app {
@@ -66,31 +44,5 @@ export default {
   margin: auto;
   display: flex;
   justify-content: center;
-}
-
-.side-by-side {
-  display: flex;
-  align-items: flex-start;
-}
-
-pre.range-output {
-  background: #eee;
-  border-radius: 0.5em;
-  text-align: center;
-  margin-left: 1em;
-  margin-top: 1.5em;
-  font-weight: bold;
-  padding: 0.5em;
-  line-height: 1;
-  width: 1.5em;
-}
-
-pre.form-data {
-  box-sizing: border-box;
-  background: #eee;
-  border: 1px solid #ccc;
-  width: 100%;
-  padding: 1em;
-  border-radius: 0.5em;
 }
 </style>
