a84f5a25d680224f2fd6808ccb54feee9ef5b877
Gustavo Fenilli <gustavofenilli@hotmail.com>
2022-03-08 10:59:05 -0300

    adds custom attributes and message states by theme

--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -5,18 +5,20 @@ import plugin from 'tailwindcss/plugin'
  * The FormKit plugin for Tailwind
  * @public
  */
-const formKitVariants = plugin(({ addVariant }) => {
-  addVariant('formkit-disabled', ['&[data-disabled]', '[data-disabled] &', '[data-disabled]&'])
-  addVariant('formkit-invalid', ['&[data-invalid]', '[data-invalid] &', '[data-invalid]&'])
-  addVariant('formkit-errors', ['&[data-errors]', '[data-errors] &', '[data-errors]&'])
-  addVariant('formkit-complete', ['&[data-complete]', '[data-complete] &', '[data-complete]&'])
-  addVariant('formkit-loading', ['&[data-loading]', '[data-loading] &', '[data-loading]&'])
-  addVariant('formkit-submitted', ['&[data-submitted]', '[data-submitted] &', '[data-submitted]&'])
-  addVariant('formkit-multiple', ['&[data-multiple]', '[data-multiple] &', '[data-multiple]&'])
-  addVariant('formkit-action', ['.formkit-actions &', '.formkit-actions&'])
-  addVariant('formkit-message-validation', ['[data-message-type="validation"] &', '[data-message-type="validation"]&'])
-  addVariant('formkit-message-error', ['[data-message-type="error"] &', '[data-message-type="error"]&'])
-})
+const formKitVariants = plugin(({ addVariant, theme }) => {
+  const attributes: string[] = theme('formkit.attributes') || [];
+  const messageStates: string[] = theme('formkit.messageStates') || [];
+
+  addVariant('formkit-action', ['.formkit-actions &', '.formkit-actions&']);
+
+  ['disabled', 'invalid', 'errors', 'complete', 'loading', 'submitted', 'multiple', ...attributes].forEach((attribute) => {
+    addVariant(`formkit-${attribute}`, [`&[data-${attribute}]`, `[data-${attribute}] &`, `[data-${attribute}]&`])
+  });
+
+  ['validation', 'error', ...messageStates].forEach((state) => {
+    addVariant(`formkit-message-${state}`, [`[data-message-type="${state}"] &`, `[data-message-type="${state}"]&`])
+  });
+});
 
 /**
  * An object of ClassFunctions
