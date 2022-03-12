f336b3eff92e0b11e23c7238cf4befa49bb362bf
daniil4udo <daniil4udo@gmail.com>
2022-03-12 17:53:24 +0400

    feat: add auoimport to the nuxt plugin

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,9 +1,46 @@
 import { existsSync } from 'fs'
 import { fileURLToPath } from 'url'
-import { defineNuxtModule, addPluginTemplate, createResolver } from '@nuxt/kit'
+
+import * as formkit from '@formkit/vue'
+import { defineNuxtModule, addPluginTemplate, createResolver, extendViteConfig } from '@nuxt/kit'
 
 export interface ModuleOptions {
+  autoImports?: {
+    /**
+     * Prefix to be added before every imports function.
+     * False to disable prefix
+
+     * @defaultValue `use`
+     */
+    prefix?: string
+    /**
+     * Functions that starts with keywords in this array will be skipped by prefix
+
+    * @defaultValue [`fk`]
+     */
+    prefixSkip?: string[]
+    /**
+     * Array of imports to be exluded from auto-imports
+
+    * @defaultValue [`FormKit`, `FormKitSchema`]
+     */
+    exclude?: string[]
+    /**
+     * Iterable of string pairs to alias each function
+
+    * @defaultValue []
+     */
+    alias?: Iterable<[string, string]>
+  }
+
+  /**
+   * @defaultValue true
+   */
   defaultConfig: boolean
+
+  /**
+   * @defaultValue undefined
+   */
   configFile?: string
 }
 
@@ -16,6 +53,12 @@ export default defineNuxtModule<ModuleOptions>({
     },
   },
   defaults: {
+    autoImports: {
+      prefix: 'fk',
+      prefixSkip: ['is'],
+      exclude: ['FormKit', 'FormKitSchema'],
+      alias: [],
+    },
     defaultConfig: true,
     configFile: undefined,
   },
@@ -52,5 +95,47 @@ export default defineNuxtModule<ModuleOptions>({
       filename: 'formkitPlugin.mjs',
       options: { importStatement, config },
     })
+
+    if (options.autoImports) {
+      const imports = []
+      const prefix = options.autoImports.prefix || ''
+      const aliasMap = new Map(options.autoImports.alias)
+      const exludes = options.autoImports.exclude
+
+      for (const [name] of Object.entries(formkit)) {
+        if (!exludes.includes(name)) {
+          const alias = aliasMap.has(name) ? aliasMap.get(name) : name
+          const as = (() => {
+            const isPrefix = !options.autoImports.prefixSkip.some((key) => alias.startsWith(key)) && prefix
+            return isPrefix ? prefix + alias.charAt(0).toUpperCase() + alias.slice(1) : alias;
+          })() as string
+          imports.push({ name, as })
+        }
+      }
+
+      extendViteConfig((config) => {
+        config.optimizeDeps ||= {}
+        config.optimizeDeps.exclude ||= []
+        config.optimizeDeps.exclude.push('@formkit/vue')
+      })
+
+      nuxt.hook('autoImports:sources', (sources) => {
+        if (sources.find(i => i.from === '@formkit/vue'))
+          return
+
+        // REmove when Nuxt adds "imports" to types
+        // @ts-ignore
+        sources.push({ imports, from: '@formkit/vue' })
+      })
+    }
   },
 })
+
+declare module '@nuxt/schema' {
+  interface NuxtConfig {
+    formkit?: ModuleOptions
+  }
+  interface NuxtOptions {
+    formkit?: ModuleOptions
+  }
+}
