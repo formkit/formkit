d9185b5b7dc52c4f72b437cf269595a9651193e9
Andrew Boyd <andrew@wearebraid.com>
2022-05-26 17:42:13 -0400

    re-adds all icon files (casing fix)

---
+++ packages/icons/src/icons/application/index.ts
@@ -0,0 +1,93 @@
+import add from './Add'
+import avatarMan from './AvatarMan'
+import avatarWoman from './AvatarWoman'
+import bookmark from './Bookmark'
+import close from './Close'
+import exportAction from './Export'
+import eye from './Eye'
+import eyeClosed from './EyeClosed'
+import flag from './Flag'
+import happy from './Happy'
+import heart from './Heart'
+import help from './Help'
+import info from './Info'
+import linkExternal from './LinkExternal'
+import link from './Link'
+import megaphone from './Megaphone'
+import open from './Open'
+import people from './People'
+import refresh from './Refresh'
+import reorder from './Reorder'
+import reply from './Reply'
+import sad from './Sad'
+import magnifyingGlass from './Search'
+import settings from './Settings'
+import share from './Share'
+import start from './Start'
+import tag from './Tag'
+import tools from './Tools'
+import trash from './Trash'
+import volumeDown from './VolumeDown'
+import volumeUp from './VolumeUp'
+import warning from './Warning'
+
+// ported from "video" category
+import compress from './Compress'
+import expand from './Expand'
+import fastForward from './FastForward'
+import minimize from './Minimize'
+import pause from './Pause'
+import playCircle from './PlayCircle'
+import play from './Play'
+import rewind from './Rewind'
+import stepBack from './StepBack'
+import stepForward from './StepForward'
+import stop from './Stop'
+
+
+export {
+  add,
+  avatarMan,
+  avatarWoman,
+  bookmark,
+  close,
+  exportAction,
+  eye,
+  eyeClosed,
+  flag,
+  happy,
+  heart,
+  help,
+  info,
+  linkExternal,
+  link,
+  megaphone,
+  open,
+  people,
+  refresh,
+  reorder,
+  reply,
+  sad,
+  magnifyingGlass,
+  settings,
+  share,
+  start,
+  tag,
+  tools,
+  trash,
+  volumeDown,
+  volumeUp,
+  warning,
+  // ported from "video" category
+  compress,
+  expand,
+  fastForward,
+  minimize,
+  pause,
+  playCircle,
+  play,
+  rewind,
+  stepBack,
+  stepForward,
+  stop
+}
