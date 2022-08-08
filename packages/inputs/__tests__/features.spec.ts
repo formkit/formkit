6b32d9f0c9b3fd34390a053f46c98411ea28d026
Justin Schroeder <justin@formkit.com>
2022-08-08 12:29:49 -0400

    Fixes tests related to options function

--- packages/inputs/__tests__/features.spec.ts
+++ packages/inputs/__tests__/features.spec.ts
@@ -32,36 +32,7 @@ describe('options', () => {
       },
     ])
   })
-  it('can take a function and use its return value if not a promise', () => {
-    const node = createNode({
-      plugins: [
-        createLibraryPlugin({
-          foo: {
-            type: 'input',
-            features: [formatsOptions],
-            props: ['options'],
-          },
-        }),
-      ],
-      props: {
-        type: 'foo',
-        attrs: {
-          options: () => ['foo', 'bar'],
-        },
-      },
-    })
-    expect(node.props.options).toEqual([
-      {
-        label: 'foo',
-        value: 'foo',
-      },
-      {
-        label: 'bar',
-        value: 'bar',
-      },
-    ])
-  })
-  it('can take a function and return an empty array if the function returns a promise', () => {
+  it('can take a function and sets the optionsLoader prop and assigns an empty array', () => {
     const node = createNode({
       plugins: [
         createLibraryPlugin({
