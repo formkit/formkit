362f376ba242de0663ca7cc0cf1de14e881f4aef
Justin Schroeder <justin@formkit.com>
2022-08-26 15:35:12 -0400

    Prop override test

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -28,7 +28,7 @@ const config = defaultConfig({
   locale: 'en',
   theme: 'genesis',
   icons: {
-    formkit: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 0.0182495H0V4.01533H4V8.01167L7.9989 8.01167V12.0088H4V16.0058H0V20.0029H4V16.0058H8V12.0088H11.9989V8.01167L8 8.01167V4.01459H4V0.0182495ZM11.9983 20.0029H15.9977H15.9983H19.9972H19.9977H23.9972V24H19.9977H19.9972H15.9983H15.9977H11.9983V20.0029Z" fill="currentColor"/></svg>`
+    formkit: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 0.0182495H0V4.01533H4V8.01167L7.9989 8.01167V12.0088H4V16.0058H0V20.0029H4V16.0058H8V12.0088H11.9989V8.01167L8 8.01167V4.01459H4V0.0182495ZM11.9983 20.0029H15.9977H15.9983H19.9972H19.9977H23.9972V24H19.9977H19.9972H15.9983H15.9977H11.9983V20.0029Z" fill="currentColor"/></svg>`,
   },
   inputs: { foo: myInput },
   plugins: [createAutoAnimatePlugin()],
@@ -59,7 +59,7 @@ const router = createRouter({
     },
     {
       path: '/formkit-icon-component',
-      component: FormKitIconExample
+      component: FormKitIconExample,
     },
     {
       path: '/custom-input',
