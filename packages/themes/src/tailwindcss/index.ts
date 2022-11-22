2b86901421152abfa56eb1376a0b6ff6b3912483
GustavoFenilli <gustavofenilli@gmail.com>
2022-11-22 12:44:59 -0300

    removes @types/tailwindcss and fixes ts error

--- packages/themes/src/tailwindcss/index.ts
+++ packages/themes/src/tailwindcss/index.ts
@@ -16,13 +16,12 @@ const outerAttributes = [
  * The FormKit plugin for Tailwind
  * @public
  */
-// @ts-expect-error matchVariant is not documented or have types
 const FormKitVariants = plugin(function ({ matchVariant }) {
   const attributes = outerAttributes.reduce((a, v) => ({ ...a, [v]: v }), {})
 
   matchVariant(
     'formkit',
-    (value = '', { modifier }: { modifier: string }) => {
+    (value = '', { modifier }) => {
       return modifier
         ? [
             `[data-${value}='true']:merge(.group\\/${modifier})&`,
