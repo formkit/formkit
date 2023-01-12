3b2835c2f80759556f36a7e85e3e1b80359e003d
Justin Schroeder <justin@formkit.com>
2023-01-12 14:19:54 -0500

    Adds a test to check for unignorability

--- packages/inputs/src/sections/submitInput.ts
+++ packages/inputs/src/sections/submitInput.ts
@@ -7,7 +7,6 @@ export const submitInput = createSection('submit', () => ({
   $cmp: 'FormKit',
   bind: '$submitAttrs',
   props: {
-    // ignore: true,
     type: 'submit',
     disabled: '$disabled',
     label: '$submitLabel',
