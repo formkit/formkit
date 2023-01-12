03b853b24e6dd5c141142d260e37c45594caf091
Justin Schroeder <justin@formkit.com>
2023-01-12 16:18:19 -0500

    Fixes TS error in vue bindings for classes property

--- packages/vue/src/bindings.ts
+++ packages/vue/src/bindings.ts
@@ -153,7 +153,7 @@ const vueBindings: FormKitPlugin = function vueBindings(node) {
   const classes = new Proxy(cachedClasses as Record<PropertyKey, string>, {
     get(...args) {
       const [target, property] = args
-      let className = Reflect.get(...args)
+      let className: string | null = Reflect.get(...args)
       if (!className && typeof property === 'string') {
         if (!has(target, property) && !property.startsWith('__v')) {
           const observedNode = createObserver(node)
@@ -183,7 +183,7 @@ const vueBindings: FormKitPlugin = function vueBindings(node) {
               classesPropClasses,
               sectionPropClasses
             )
-            target[property] = className
+            target[property] = className ?? ''
           })
         }
       }
