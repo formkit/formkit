98a6c816f7de9cac2fcb7a017e0a719f41180ba9
Andrew Boyd <andrew@formkit.com>
2023-01-19 15:39:07 -0500

    adds named export of genesisIcons to assist Tailwind users

---
+++ packages/icons/src/genesisIcons.ts
@@ -0,0 +1,30 @@
+import {
+  add,
+  check,
+  circle,
+  close,
+  spinner,
+  star,
+  trash,
+} from './icons/application'
+import { arrowDown, arrowUp, down } from './icons/directional'
+import { fileDoc } from './icons/file'
+
+const genesisIcons = {
+  add,
+  arrowDown,
+  arrowUp,
+  check,
+  close,
+  checkboxDecorator: check,
+  fileItem: fileDoc,
+  fileRemove: close,
+  noFiles: fileDoc,
+  radioDecorator: circle,
+  select: down,
+  spinner,
+  star,
+  trash,
+}
+
+export default genesisIcons
