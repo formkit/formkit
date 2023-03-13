ea226c383ce01dd4f01a5c95ed3f0220716c2e41
Andrew Boyd <andrew@formkit.com>
2023-03-13 17:05:10 -0400

    comment out pro plugin lines

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -29,10 +29,10 @@ const myInput = createInput(CurrencyInput)
 const app = createApp(App)
 
 // pro plugin
-const proPlugin = createProPlugin(
-  '/* put dev key here — do NOT commit and push it */',
-  inputs
-)
+// const proPlugin = createProPlugin(
+//   '/* put dev key here — do NOT commit and push it */',
+//   inputs
+// )
 
 // Configure FormKit:
 const config = defaultConfig({
@@ -43,7 +43,11 @@ const config = defaultConfig({
     formkit: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 0.0182495H0V4.01533H4V8.01167L7.9989 8.01167V12.0088H4V16.0058H0V20.0029H4V16.0058H8V12.0088H11.9989V8.01167L8 8.01167V4.01459H4V0.0182495ZM11.9983 20.0029H15.9977H15.9983H19.9972H19.9977H23.9972V24H19.9977H19.9972H15.9983H15.9977H11.9983V20.0029Z" fill="currentColor"/></svg>`,
   },
   inputs: { foo: myInput },
-  plugins: [proPlugin, createAutoAnimatePlugin(), createMultiStepPlugin()],
+  plugins: [
+    // proPlugin,
+    createAutoAnimatePlugin(),
+    createMultiStepPlugin(),
+  ],
 })
 
 // Install FormKit:
