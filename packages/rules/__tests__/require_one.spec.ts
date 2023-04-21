ce5efb9748aed8a3c6a8cfc2b86f49b5baa0bc6e
Chris Ellinger <cellinger@wearebraid.com>
2023-04-21 14:00:18 -0400

    Adds some testing for require_one validation rule

--- packages/rules/__tests__/require_one.spec.ts
+++ packages/rules/__tests__/require_one.spec.ts
@@ -3,10 +3,39 @@ import require_one from '../src/require_one'
 import { describe, expect, it } from 'vitest'
 
 describe('require_one rule', () => {
+
   it('fails on an empty string', () =>
     expect(require_one(createNode({ value: '' }))).toBe(false))
 
   it('fails on an empty string', () =>
     expect(require_one(createNode({ value: 'test' }))).toBe(true))
+
+  it('can infer a sibling has value', () => {
+    const form = createNode({ type: 'group' })
+    const childA = createNode({ parent: form, name: 'foo' })
+    createNode({ parent: form, name: 'bar', value: 'baz' })
+    expect(require_one(childA, 'bar')).toBe(true)
+  })
+
+  it('fails to infer a sibling has value', () => {
+    const form = createNode({ type: 'group' })
+    const childA = createNode({ parent: form, name: 'foo' })
+    createNode({ parent: form, name: 'bar'})
+    expect(require_one(childA, 'bar')).toBe(false)
+  })
+
+  it('fails when no arguments are passed with a input with no value', () => {
+    const form = createNode({ type: 'group' })
+    const childA = createNode({ parent: form, name: 'foo' })
+    createNode({ parent: form, name: 'bar'})
+    expect(require_one(childA)).toBe(false)
+  })
+
+  it('fails when argument targets an input that does\'t exist', () => {
+    const form = createNode({ type: 'group' })
+    const childA = createNode({ parent: form, name: 'foo' })
+    createNode({ parent: form, name: 'bar', value: 'baz' })
+    expect(require_one(childA, 'baz')).toBe(false)
+  })
 
 })
