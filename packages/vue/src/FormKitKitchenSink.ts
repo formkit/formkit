064333e2f38a196d715711e1c6ab25b6534adf1c
Andrew Boyd <andrew@formkit.com>
2023-12-04 16:57:37 -0500

    feat: improves kitchen sink — including all required Tailwind styling

--- packages/vue/src/FormKitKitchenSink.ts
+++ packages/vue/src/FormKitKitchenSink.ts
@@ -2,6 +2,105 @@ import { h, defineComponent, ref, computed } from 'vue'
 import { FormKitSchemaDefinition } from '@formkit/core'
 import { FormKitSchema } from './index'
 
+let inputList: Record<string, string[]> = {}
+const schemas: Record<string, FormKitSchemaDefinition[]> = {}
+
+const classes = {
+  container: `
+    formkit-kitchen-sink 
+    p-8
+  `,
+  tabs: `
+    formkit-tabs 
+    mt-4 
+    mb-8
+  `,
+  tab: `
+    formkit-kitchen-sink-tab
+    inline-block
+    mr-4
+    cursor-pointer
+    px-4
+    py-2
+    border
+    border-neutral-100
+    text-neutral-800
+    rounded
+    data-[active]:bg-neutral-800
+    data-[active]:text-neutral-50
+    hover:bg-neutral-100
+    hover:text-neutral-900
+    dark:border-neutral-800
+    dark:text-neutral-50
+    dark:data-[active]:bg-neutral-100
+    dark:data-[active]:text-neutral-800
+    dark:hover:bg-neutral-800
+    dark:hover:text-neutral-50
+  `,
+  formContainer: `
+    formkit-form-container
+    w-full
+    bg-white
+    rounded
+    border
+    border-neutral-100
+    shadow-lg
+    max-w-[800px]
+    p-[min(5vw,5rem)]
+    dark:bg-neutral-900
+    dark:border-neutral-800
+    dark:shadow-3xl
+    [&_form>h1]:text-2xl
+    [&_form>h1]:mb-4
+    [&_form>h1]:font-bold
+    [&_form>h1+p]:text-base
+    [&_form>h1+p]:mb-4
+    [&_form>h1+p]:-mt-2
+    [&_form_.double]:flex
+    [&_form_.double>*]:w-1/2
+    [&_form_.double>*:first-child]:mr-2
+    [&_form_.triple]:flex
+    [&_form_.triple>*]:w-1/3
+    [&_form_.triple>*:first-child]:mr-2
+    [&_form_.triple>*:last-child]:ml-2
+  `,
+  inputs: `formkit-inputs`,
+  specimen: `
+    formkit-specimen 
+    flex 
+    flex-col 
+    p-2 
+    max-w-[75vw]
+  `,
+  inputSection: `
+    group/section
+    formkit-input-section 
+    mr-[min(325px,25vw)]
+  `,
+  specimenGroup: `
+    formkit-specimen-group
+    grid
+    mb-16
+    grid-cols-[repeat(auto-fit,400px)]
+    group-data-[type="transferlist"]/section:grid-cols-[repeat(auto-fit,650px)]
+    group-data-[type="multi-step"]/section:grid-cols-[repeat(auto-fit,550px)]
+  `,
+  inputType: `
+    formkit-input-type
+    block
+    font-bold
+    text-neutral-900
+    border-b
+    border-neutral-100
+    text-3xl
+    mb-8
+    pb-2
+    capitalize
+    dark:border-neutral-800 
+    dark:text-neutral-50
+  `,
+}
+
 /**
  * Fetches the list of inputs from the remote schema repository
  */
@@ -38,18 +137,31 @@ async function fetchInputSchema(input: string) {
 export const FormKitKitchenSink = /* #__PURE__ */ defineComponent({
   name: 'FormKitKitchenSink',
   props: {
-    pro: {
-      type: Boolean,
-      default: true,
-    },
     schemas: {
       type: Array,
       required: false,
     },
+    pro: {
+      type: Boolean,
+      default: true,
+    },
+    addons: {
+      type: Boolean,
+      default: true,
+    },
+    forms: {
+      type: Boolean,
+      default: true,
+    },
+    navigation: {
+      type: Boolean,
+      default: true,
+    },
   },
   async setup(props) {
-    const inputList = await fetchInputList()
-    const schemas: Record<string, FormKitSchemaDefinition[]> = {}
+    inputList = Object.keys(inputList).length
+      ? inputList
+      : await fetchInputList()
     const promises = []
     const activeTab = ref('')
 
@@ -60,12 +172,29 @@ export const FormKitKitchenSink = /* #__PURE__ */ defineComponent({
       })
       promises.push(...coreInputPromises)
 
+      if (props.forms) {
+        const formsPromises = inputList.forms.map(async (schema: string) => {
+          const schemaName = `form/${schema}`
+          const response = await fetchInputSchema(schemaName)
+          schemas[schemaName] = response
+        })
+        promises.push(...formsPromises)
+      }
+
       if (props.pro) {
         const proInputPromises = inputList.pro.map(async (schema: string) => {
           const response = await fetchInputSchema(schema)
           schemas[schema] = response
         })
         promises.push(...proInputPromises)
+      }
+
+      if (props.addons) {
+        const addonPromises = inputList.addons.map(async (schema: string) => {
+          const response = await fetchInputSchema(schema)
+          schemas[schema] = response
+        })
+        promises.push(...addonPromises)
       }
     } else {
       const schemaPromises = props.schemas.map(async (schema: unknown) => {
@@ -137,103 +266,109 @@ export const FormKitKitchenSink = /* #__PURE__ */ defineComponent({
       })
     }
     if (forms.length) {
-      tabs.push(...filteredFormNames)
+      tabs.push(...filteredFormNames.sort((a, b) => (a.name > b.name ? 1 : -1)))
     }
     if (tabs.length) {
       activeTab.value = tabs[0].id
     }
 
     // collection of all inputs to be rendered in the "kitchen sink" tab
-    const kitchenSinkRenders = inputs.map((inputName: string) => {
-      const schemaDefinitions = schemas[inputName]
-      const schemaRenders = schemaDefinitions.map(
-        (schema: FormKitSchemaDefinition) => {
-          return h(
-            'div',
-            {
-              class: 'formkit-specimen flex flex-col p-2 max-w-[75vw]',
-            },
-            [
-              h(FormKitSchema, {
-                schema: schema,
-                data: data,
-              }),
-            ]
-          )
+    const kitchenSinkRenders = computed(() => {
+      if (activeTab.value !== 'kitchen-sink') return []
+      return inputs.sort().map((inputName: string) => {
+        const schemaDefinitions = schemas[inputName]
+        if (!schemaDefinitions) {
+          return
         }
-      )
-      return h(
-        'div',
-        {
-          class: 'formkit-input-section mr-[min(350px,25vw)]',
-          'data-type': inputName,
-        },
-        [
-          h(
-            'span',
-            {
-              class: `
-                formkit-input-type block font-bold text-neutral-900 border-b border-neutral-100 text-3xl mb-8 pb-2 capitalize
-                dark:border-neutral-800 dark:text-neutral-50
-            `,
-            },
-            inputName
-          ),
-          h(
-            'div',
-            {
-              class:
-                'formkit-specimen-group grid grid-cols-[repeat(auto-fit,400px)] mb-16',
-            },
-            schemaRenders
-          ),
-        ]
-      )
+        const schemaRenders = schemaDefinitions.map(
+          (schema: FormKitSchemaDefinition) => {
+            return h(
+              'div',
+              {
+                class: classes.specimen,
+              },
+              [
+                h(FormKitSchema, {
+                  schema: schema,
+                  data: data,
+                }),
+              ]
+            )
+          }
+        )
+        return h(
+          'div',
+          {
+            key: inputName,
+            class: classes.inputSection,
+            'data-type': inputName,
+          },
+          [
+            h(
+              'span',
+              {
+                class: classes.inputType,
+              },
+              inputName
+            ),
+            h(
+              'div',
+              {
+                class: classes.specimenGroup,
+              },
+              schemaRenders
+            ),
+          ]
+        )
+      })
     })
 
     const formRenders = computed(() => {
-      return filteredFormNames.map((form) => {
-        const schemaDefinition = schemas[form.id]
-        return h(
-          'div',
-          {},
-          activeTab.value === form.id
-            ? [
-                h(
-                  'div',
-                  {
-                    class:
-                      'w-full bg-white rounded border border-neutral-100 shadow-lg max-w-[800px] p-[min(5vw,5rem)] dark:bg-neutral-900 dark:border-neutral-800 dark:shadow-3xl',
-                  },
-                  [
-                    h(FormKitSchema, {
-                      schema: schemaDefinition[0],
-                      data: data,
-                    }),
-                  ]
-                ),
-              ]
-            : ''
-        )
-      })
+      return filteredFormNames
+        .map((form) => {
+          const schemaDefinition = schemas[form.id]
+          return h(
+            'div',
+            {
+              key: form.id,
+            },
+            activeTab.value === form.id
+              ? [
+                  h(
+                    'div',
+                    {
+                      class: classes.formContainer,
+                    },
+                    [
+                      h(FormKitSchema, {
+                        schema: schemaDefinition[0],
+                        data: data,
+                      }),
+                    ]
+                  ),
+                ]
+              : ''
+          )
+        })
+        .filter((form) => form.children)
     })
 
     const tabBar = computed(() => {
       return h(
         'div',
         {
-          class: 'formkit-kitchen-sink-tabs mt-4 mb-8',
+          key: 'tab-bar',
+          class: classes.tabs,
         },
         tabs.map((tab) => {
           return h(
             'span',
             {
-              class:
-                'formkit-kitchen-sink-tab inline-block mr-4 cursor-pointer px-4 py-2 border border-neutral-100 text-neutral-800 rounded data-[active]:bg-neutral-800 data-[active]:text-neutral-50 hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:text-neutral-50 dark:data-[active]:bg-neutral-100 dark:data-[active]:text-neutral-800 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
+              class: classes.tab,
+              key: tab.id,
               'data-tab': tab.id,
               'data-active': activeTab.value === tab.id || undefined,
               onClick: () => {
-                console.log(tab.id)
                 activeTab.value = tab.id
               },
             },
@@ -247,20 +382,12 @@ export const FormKitKitchenSink = /* #__PURE__ */ defineComponent({
       return h(
         'div',
         {
-          class: 'formkit-kitchen-sink my-12',
+          class: classes.container,
         },
         [
-          tabs.length > 1 ? tabBar.value : '',
-          h(
-            'div',
-            {
-              class: 'formkit-inputs',
-            },
-            kitchenSinkRenders.length && activeTab.value === 'kitchen-sink'
-              ? kitchenSinkRenders
-              : []
-          ),
+          tabs.length > 1 ? tabBar.value : undefined,
           ...formRenders.value,
+          ...kitchenSinkRenders.value,
         ]
       )
     }
