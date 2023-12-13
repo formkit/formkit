37afdf201a115900528811ec4428a6cfd7dd6b45
Andrew Boyd <andrew@formkit.com>
2023-12-13 11:42:08 -0500

    Updates floating labels to work with new tailwind themes — light mode / dark mode

--- packages/addons/src/plugins/floatingLabels/floatingLabelsPlugin.ts
+++ packages/addons/src/plugins/floatingLabels/floatingLabelsPlugin.ts
@@ -4,7 +4,7 @@ import {
   FormKitSchemaNode,
   FormKitSchemaCondition,
 } from '@formkit/core'
-import { clone } from '@formkit/utils'
+import { clone, whenAvailable } from '@formkit/utils'
 import { findSection } from '@formkit/inputs'
 
 /**
@@ -14,6 +14,55 @@ import { findSection } from '@formkit/inputs'
  */
 export interface FloatingLabelsOptions {
   useAsDefault?: boolean
+}
+
+/**
+ * Traverses through parent elements to find the next closest non-transparent assigned background color
+ * @param element - The element to start the search from
+ */
+function findParentWithBackgroundColor(element: HTMLElement): string {
+  let backgroundColor = 'white'
+  while (backgroundColor === 'white' && element.parentElement) {
+    element = element.parentElement
+    const style = window.getComputedStyle(element)
+    const bgColor = style.backgroundColor
+
+    if (
+      bgColor &&
+      bgColor !== 'rgba(0, 0, 0, 0)' &&
+      bgColor !== 'transparent'
+    ) {
+      backgroundColor = bgColor
+    }
+    // Check if the color uses CSS variable for opacity
+    const opacityMatch = backgroundColor.match(/var\(([^)]+)\)/)
+    if (opacityMatch) {
+      const opacityVar = opacityMatch[1]
+      const opacity =
+        getComputedStyle(document.documentElement)
+          .getPropertyValue(opacityVar)
+          .trim() || '1'
+      backgroundColor = `rgba(${bgColor}, ${opacity})`
+    }
+    console.log(backgroundColor)
+  }
+  return backgroundColor
+}
+
+/**
+ * Sets the background color of the label to the background color of the parent element
+ * @param node - The node to set the background color for
+ * @param nodeRoot - The root node to start the search from
+ * @param timeout - The timeout to wait for the background color to be set
+ */
+function setBackgroundColor(
+  node: FormKitNode,
+  nodeRoot: HTMLElement,
+  timeout: number
+) {
+  setTimeout(() => {
+    node.props._labelBackgroundColor = findParentWithBackgroundColor(nodeRoot)
+  }, timeout)
 }
 
 /**
@@ -29,7 +78,7 @@ export function createFloatingLabelsPlugin(
   FloatingLabelsOptions?: FloatingLabelsOptions
 ): FormKitPlugin {
   const floatingLabelsPlugin = (node: FormKitNode) => {
-    node.addProps(['floatingLabel'])
+    node.addProps(['floatingLabel', '_labelBackgroundColor'])
 
     const useFloatingLabels =
       typeof node.props.floatingLabel === 'boolean'
@@ -38,7 +87,15 @@ export function createFloatingLabelsPlugin(
         ? FloatingLabelsOptions?.useAsDefault
         : false
 
-    if (useFloatingLabels) {
+    if (useFloatingLabels && node.context) {
+      // use a mouseEnter event to trigger a calculation of the background color
+      whenAvailable(node.context.id, () => {
+        if (!node.context) return
+        const nodeEl = document.getElementById(node.context?.id)
+        if (!nodeEl) return
+        setBackgroundColor(node, nodeEl, 100)
+      })
+
       node.on('created', () => {
         if (!node.props || !node.props.definition) return
         const inputDefinition = clone(node.props.definition)
@@ -62,6 +119,9 @@ export function createFloatingLabelsPlugin(
             extensions.label = {
               attrs: {
                 'data-has-value': '$_value !== "" && $_value !== undefined',
+                style: '$: "background-color: " + $_labelBackgroundColor',
+                'data-style':
+                  '$: "background-color: " + $_labelBackgroundColor',
               },
             }
 
@@ -73,7 +133,12 @@ export function createFloatingLabelsPlugin(
               finalSchema,
               'label'
             )
-            const [inputParentChildren] = findSection(finalSchema, 'input')
+            const targetSection =
+              node.props.type === 'dropdown' ? 'selector' : 'input'
+            const [inputParentChildren] = findSection(
+              finalSchema,
+              targetSection
+            )
 
             if (labelParentChildren && labelSection && inputParentChildren) {
               labelParentChildren.splice(
