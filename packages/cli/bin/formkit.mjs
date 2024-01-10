335e54f015fd54ce58076423b94fa33ff3001515
Justin Schroeder <justin@formkit.com>
2024-01-10 12:29:09 -0500

    fix: changes bin to use exported cli name

--- packages/cli/bin/formkit.mjs
+++ packages/cli/bin/formkit.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import jiti from 'jiti'
-import { fileURLToPath } from "node:url";
-const __filename = fileURLToPath(import.meta.url);
+import { fileURLToPath } from 'node:url'
+const __filename = fileURLToPath(import.meta.url)
 
-jiti(__filename)('../dist/index.mjs').default()
+jiti(__filename)('../dist/index.mjs').cli()
