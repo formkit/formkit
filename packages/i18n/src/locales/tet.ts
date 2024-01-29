1686886131acfb9672043f653a325a04ccdbab54
Justin Schroeder <justin@formkit.com>
2024-01-29 16:27:12 -0500

    fix: refactor i18n exports for esbuild treeshaking (#1139)

--- packages/i18n/src/locales/tet.ts
+++ packages/i18n/src/locales/tet.ts
@@ -12,7 +12,7 @@ import { FormKitLocaleMessages } from '../i18n'
  * Standard language for interface features.
  * @public
  */
-export const ui: FormKitLocaleMessages = {
+const ui: FormKitLocaleMessages = {
   /**
    * Shown on a button for adding additional items.
    */
@@ -91,7 +91,7 @@ export const ui: FormKitLocaleMessages = {
  * These are all the possible strings that pertain to validation messages.
  * @public
  */
-export const validation: FormKitValidationMessages = {
+const validation: FormKitValidationMessages = {
   /**
    * The value is not an accepted value.
    * @see {@link https://formkit.com/essentials/validation#accepted}
@@ -382,3 +382,5 @@ export const validation: FormKitValidationMessages = {
    */
   invalidDate: 'Data la loos.',
 }
+
+export const tet = { ui, validation }
--- packages/i18n/src/locales/uz.ts
+++ packages/i18n/src/locales/uz.ts
@@ -15,7 +15,7 @@ import { FormKitLocaleMessages } from '../i18n'
  * Standard language for interface features.
  * @public
  */
-export const ui: FormKitLocaleMessages = {
+const ui: FormKitLocaleMessages = {
   /**
    * Shown on a button for adding additional items.
    */
@@ -98,7 +98,7 @@ export const ui: FormKitLocaleMessages = {
  * These are all the possible strings that pertain to validation messages.
  * @public
  */
-export const validation: FormKitValidationMessages = {
+const validation: FormKitValidationMessages = {
   /**
    * The value is not an accepted value.
    * @see {@link https://formkit.com/essentials/validation#accepted}
@@ -515,3 +515,5 @@ export const validation: FormKitValidationMessages = {
    */
   invalidDate: 'Tanlangan sana yaroqsiz.',
 }
+
+export const uz = { ui, validation }
