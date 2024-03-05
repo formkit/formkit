afa26a2bbb55b0d2f444d40532ddaee007a3a239
Daniel Roe <daniel@roe.dev>
2024-03-05 23:10:24 +0000

    test: revert change to module

--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss'],
+  modules: ['../src/module', '@nuxtjs/tailwindcss'],
   formkit: {
     autoImport: true,
   },
