d12916a2000fb054dcf42d15c91bc1d921009f50
Marven Thieme <thieme@webhub.de>
2025-02-26 09:20:44 +0100

    Using Math.min(first, second) and Math.max(first, second)

--- packages/i18n/src/locales/hr.ts
+++ packages/i18n/src/locales/hr.ts
@@ -352,8 +352,8 @@ const validation: FormKitValidationMessages = {
    * @see {@link https://formkit.com/essentials/validation#length}
    */
   length({ name, args: [first = 0, second = Infinity] }) {
-    const min = Number(first) <= Number(second) ? first : second
-    const max = Number(second) >= Number(first) ? second : first
+    const min = Math.min(first, second)
+    const max = Math.max(first, second)
     if (min == 1 && max === Infinity) {
       /* <i18n case="Shown when the length of the user-provided value is not at least one character."> */
       return `${s(name)} mora sadržavati barem jedan znak.`
